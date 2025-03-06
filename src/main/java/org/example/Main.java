2fb58cb6a98c7eb1022eeb93039c0091af795105
mertgundogann <merttgundogan@hotmail.com>
2025-03-06 09:03:50 +0300

    Person

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+    Person person1 = new Person("John", "Doe", 20);
+    Person person2 = new Person("Mert", "Gundogan", 20);
+
+
+
+
+
 }
